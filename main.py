444ca4d9f1df2102aa3d09f4546c2c21f8a2a57d
tod619 <trevorodwyer619@hotmail.com>
2023-07-06 22:06:49 +0100

    create start_timer func in main.py file

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_timer():
+    count_down(5)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
@@ -39,10 +43,10 @@ timer_text = canvas.create_text(100, 130, text="00:00", fill="white",
 canvas.grid(column=1, row=1)
 # ✔
 
-count_down(5)
 
-start_button = Button(text="Start", highlightthickness=0)
+start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
+
 reset_button = Button(text="Reset", highlightthickness=0)
 reset_button.grid(column=2, row=2)
 
