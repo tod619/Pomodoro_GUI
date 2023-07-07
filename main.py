36f922ba2556b0995209310daf8a75eb23bed719
tod619 <trevorodwyer619@hotmail.com>
2023-07-07 22:19:27 +0100

    Use different timings for work, break, & long break in main.py file

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
@@ -16,7 +17,23 @@ LONG_BREAK_MIN = 20
 
 
 def start_timer():
-    count_down(5 * 60)
+
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        # if it's 8th rep use long_break_sec
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        # if it's 2nd/4th/6th rep use short_break_sec
+        count_down(short_break_sec)
+    else:
+        # if it's 1st/3rd/5th/7th rep use work_sec
+        count_down(work_sec)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
@@ -31,6 +48,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_minute}:{count_seconds}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
