e724e686efdff845c43b8a7647faab02e96a8821
tod619 <trevorodwyer619@hotmail.com>
2023-07-08 23:07:33 +0100

    Reset text labels, + check marks + reps in reset_timer func in main.py file

--- main.py
+++ main.py
@@ -17,6 +17,11 @@ timer = None
 
 def reset_timer():
     window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_label.config(text="Timer")
+    check_marks.config(text="")
+    global reps
+    reps = 0
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
