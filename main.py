5ea881f3aa8fa337d7f50e0941704f743c27d420
tod619 <trevorodwyer619@hotmail.com>
2023-07-08 23:01:44 +0100

    create reset_timer function and clear timer when reset_button is clicked in main.py file

--- main.py
+++ main.py
@@ -10,8 +10,13 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
 
 # ---------------------------- TIMER RESET ------------------------------- #
+
+
+def reset_timer():
+    window.after_cancel(timer)
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
@@ -50,7 +55,8 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_minute}:{count_seconds}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ""
@@ -80,7 +86,7 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
 check_marks = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME, 15))
